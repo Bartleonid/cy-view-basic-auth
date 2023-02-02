641054ab8e783305087faeffd31fc4c8534535db
PXY admin <pxyadmin1@PROXYMITYLAP65.local>
2023-02-02 23:32:33 +0000

    basic-auth

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,14 @@
 /// <reference types="cypress" />
 /// <reference types="node" />
 function prepareTestsForDevices(deviceList) {
-    return function setPagesToTest(pageList, callback, auth = null) {
+    return function setPagesToTest(pageList, callback, auth) {
         pageList.forEach(function (page) {
             context("" + page, function () {
                 deviceList.forEach(function (device) {
                     context("Testing on " + device.model, function () {
                         beforeEach(function () {
                             cy.viewport(device.width, device.height);
+                            auth = auth || null;
                             cy.visit(page, auth);
                         });
                         callback();
